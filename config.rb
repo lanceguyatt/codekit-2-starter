e6ec54fdc42af74e0b772c4a0ee215e200114936
Lance Guyatt <lance@lanceguyatt.com>
2014-03-13 01:11:31 +0000

    updates

--- config.rb
+++ config.rb
@@ -9,8 +9,7 @@ images_dir = 'dist/assets/images'
 javascripts_dir = 'dist/assets/javascripts/js'
 fonts_dir = 'dist/assets/fonts'
 
-#add_import_path 'src/stylesheets/scss/_helpers/mixins'
-
 output_style = (environment == :development) ? :expanded : :compressed
 line_comments = (environment == :development) ? false : false
 sass_options = (environment == :development && firesass == true) ? { :debug_info => true } : {}
+
